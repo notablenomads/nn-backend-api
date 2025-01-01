ee9c30a350c9cd0f6c59cf4677dc63542cff6281
Mahdi Rashidi <m8rashidi@gmail.com>
2025-01-01 14:24:37 +0300

    refactor: remove Kubernetes configuration files for nestifined-ms-framework
    - Deleted Kubernetes configuration files including ConfigMap, Deployment, Service, and Kustomization for the nestifined-ms-framework across all environments (development, production, staging).
    - This cleanup simplifies the application structure by removing unused Kubernetes resources, enhancing maintainability.

--- test/mocks/base.mock.ts
+++
@@ -1,7 +0,0 @@
-import { mock, MockProxy } from 'jest-mock-extended';
-
-export const createMockEntity = <T>(entity: new () => T, properties: Partial<T>): T => {
-  const entityMock: MockProxy<T> = mock<T>();
-  Object.assign(entityMock, properties);
-  return entityMock;
-};
