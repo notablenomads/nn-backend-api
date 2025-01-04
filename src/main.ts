0b229496426bca4ae86449fc365da1b29a9219b3
Mahdi Rashidi <m8rashidi@gmail.com>
2025-01-04 14:59:12 +0300

    refactor: remove custom logger implementation and use NestJS Logger

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,31 @@
 import helmet from 'helmet';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { ClassSerializerInterceptor, ValidationPipe, VersioningType } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app/app.module';
 import { AllExceptionsFilter } from './app/core/filters/all-exceptions.filter';
-import logger from './logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
   const environment = config.get('app.env');
   const apiPrefix = config.get('app.apiPrefix');
+  const logger = new Logger('NN Backend API');
 
   app.enableShutdownHooks();
   app.use(helmet());
   app.enableCors({ origin: true, credentials: true });
-  app.setGlobalPrefix(apiPrefix, {
-    exclude: ['/'],
-  });
-  app.enableVersioning({
-    type: VersioningType.URI,
-  });
+  app.setGlobalPrefix(apiPrefix, { exclude: ['/'] });
+  app.enableVersioning({ type: VersioningType.URI });
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
+      transformOptions: { enableImplicitConversion: true },
     }),
   );
 
@@ -50,9 +45,9 @@ async function bootstrap() {
   await app.listen(config.get('app.port'), config.get('app.host'));
   const appUrl = await app.getUrl();
   const docsUrl = `${appUrl}/${apiPrefix}/docs`;
-  logger.info(`Application is running on: ${appUrl}`);
-  logger.info(`Environment: ${environment}`);
-  logger.info(`API Documentation available at: ${docsUrl}`);
+  logger.log(`Application is running on: ${appUrl}`);
+  logger.log(`Environment: ${environment}`);
+  logger.log(`API Documentation available at: ${docsUrl}`);
 }
 
 process.nextTick(bootstrap);
